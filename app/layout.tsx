34d91b866df869c41f30b095139a62f2341b201d
Mr.Quantum_1915 <darshanpatelgdh@gmail.com>
2026-01-30 21:14:58 +0530

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-// import { SpeedInsights } from '@vercel/speed-insights/next';
-// import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 import { Orbitron, Fira_Code } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/navbar';
@@ -30,8 +30,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <Navbar />
                 {children}
                 <ConditionalFooter />
-                {/* <Analytics />
-                <SpeedInsights /> */}
+                <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
