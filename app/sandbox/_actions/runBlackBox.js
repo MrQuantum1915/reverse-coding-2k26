9ff02bb01b7880e9ac3d8c8fb6f170a26e2f126d
Mr.Quantum_1915 <darshanpatelgdh@gmail.com>
2026-02-01 13:35:12 +0530

    fix the undefined output, by keeping the pure number inputs as string

--- app/sandbox/_actions/runBlackBox.js
+++ app/sandbox/_actions/runBlackBox.js
@@ -1,5 +1,6 @@
 import vm from 'vm';
 import { createClient } from '@supabase/supabase-js';
+import { output } from 'framer-motion/client';
 
 //cache the scripts for problems (reduce latency of fetching from DB) => persist in RAM untill server restart - on vercel untill cold restart, like after 3-5 mins of inactivity vercel kill the container on hobby plan (no gurantees :/)
 
@@ -12,12 +13,8 @@ const supabase = createClient(
 );
 
 function parseInput(val) {
-    // Check if it's a pure number
-    const num = Number(val);
-    if (!isNaN(num)) return num;
-
     // Check if it's JSON (Array/Object)
-    if (val.startsWith('[') || val.startsWith('{')) {
+    if (typeof val === 'string' && (val.trim().startsWith('[') || val.trim().startsWith('{'))) {
         try {
             return JSON.parse(val);
         } catch {
@@ -80,7 +77,7 @@ export async function runBlackBox(input, problemId) {
         // script is already a vm.Script - parsed and compiled, so this just runs it and its fast, as the compiled bytecode is cached    
         // time limit 50ms execution time to avoid infinite loops and brute force input space
         script.runInContext(context, { timeout: 50 });
-
+        console.log("result:", sandbox.result);
         return { output: sandbox.result };
     } catch (err) {
         if (err.message?.includes('timed out')) {
