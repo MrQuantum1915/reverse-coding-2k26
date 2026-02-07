d670a436926432a134f30afb386c2915902fc899
Mr.Quantum_1915 <darshanpatelgdh@gmail.com>
2026-02-07 14:03:51 +0530

    implemented upstash redis for rate liminting, as lru cache failed on vercel serverless and aggresive resource killing on hobby plan , even on single concurrent user!

--- app/sandbox/_actions/checkRateLimit.js
+++ app/sandbox/_actions/checkRateLimit.js
@@ -1,19 +1,21 @@
-import { LRUCache } from "lru-cache";
+import { ratelimit } from "@/lib/redis-ratelimit";
 
-// LRUcache<string, number>
-
-const RateLimitCache = new LRUCache({
-    max: 1000, // max 1000 users tracked in RAM
-    ttl: 1000, // 1 sec - if user makes another request within 1 sec, block it
-    ttlResolution: 1,
-});
 export async function checkRateLimit(userId) {
- 
-    if(RateLimitCache.has(userId)){
-        return false;
-    }
-    else{
-        RateLimitCache.set(userId, 1);
+    
+    const identifier = `blackbox_limit:${userId}`;
+
+    try {
+        const { success, limit, reset, remaining } = await ratelimit.limit(identifier);
+
+        if (!success) {
+            console.warn(`[RateLimit] BLOCKED user ${userId}. Resets in ${reset - Date.now()}ms`);
+            return false;
+        }
+
         return true;
+    } catch (error) {
+        // return true on error to avoid blocking users due to Redis issue
+        console.error("[RateLimit] Redis Error:", error);
+        return true; 
     }
 }
